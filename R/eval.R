b7dd37bd2dddcc6b57519d1d1c79f6c62e02d530
Alexandre Wadoux <33750884+AlexandreWadoux@users.noreply.github.com>
2020-02-12 18:16:33 +1100

    Update eval.R

--- R/eval.R
+++ R/eval.R
@@ -15,18 +15,18 @@ eval <- function(obs, pred, obj){
   if(obj == 'quant'){
 
     # mean error
-    ME <- mean(pred - obs, na.rm = TRUE)
+    ME <- round(mean(pred - obs, na.rm = TRUE), digits = 2)
 
     # root mean square error
-    RMSE <-   sqrt(mean((pred - obs)^2, na.rm = TRUE))
+    RMSE <-   round(sqrt(mean((pred - obs)^2, na.rm = TRUE)), digits = 2)
 
     # Pearson's correlation squared
-    r2 <-  cor(pred, obs, method = 'spearman', use = 'pairwise.complete.obs')^2
+    r2 <-  round((cor(pred, obs, method = 'spearman', use = 'pairwise.complete.obs')^2), digits = 2)
 
     # coefficient of determination
     SSE <- sum((pred - obs) ^ 2, na.rm = T)
     SST <- sum((obs - mean(obs, na.rm = T)) ^ 2, na.rm = T)
-    R2 <- 1 - SSE/SST
+    R2 <- round((1 - SSE/SST), digits = 2)
 
     # concordance correlation coefficient
     n <- length(pred)
@@ -41,19 +41,19 @@ eval <- function(obs, pred, obj){
     u <- (mean(pred, na.rm = T) - mean(obs, na.rm = T)) / ((sPred2 * sObs2)^0.25)
     Cb <- ((v + 1 / v + u^2)/2)^-1
     rCb <- r * Cb
-    rhoC <- rCb
+    rhoC <- round(rCb, digits = 2)
 
     # RPD
     sdObs <- sd(obs)
     RMSE_2 <- sqrt(mean((pred - obs)^2))
-    RPD <- sdObs/RMSE_2
+    RPD <- round((sdObs/RMSE_2), digits = 2)
 
     # RPIQ
     q25 <- as.numeric(quantile(obs)[2])
     q75 <- as.numeric(quantile(obs)[4])
     iqDist <- q75 - q25
     RMSE <- sqrt(mean((pred - obs)^2))
-    rpiq <- iqDist/RMSE
+    rpiq <- round((iqDist/RMSE), digits = 2)
 
     # return the results
     evalRes <- data.frame(ME = ME, RMSE = RMSE, r2 = r2, R2 = R2, RPD = RPD, RPIQ = rpiq)
@@ -64,7 +64,7 @@ eval <- function(obs, pred, obj){
     cm = as.matrix(table(obs = obs, pred = pred))
     n <- length(obs)
     diag = diag(cm)
-    OA <- sum(diag) / n
+    OA <- round((sum(diag) / n), digits = 2)
 
     # Cohens' kappa
     cm = as.matrix(table(obs = obs, pred = pred))
@@ -76,7 +76,7 @@ eval <- function(obs, pred, obj){
     p = rowsums / n
     q = colsums / n
     expAccuracy = sum(p*q)
-    kappa = (accuracy - expAccuracy) / (1 - expAccuracy)
+    kappa = round(((accuracy - expAccuracy) / (1 - expAccuracy)), digits = 2)
 
     # return the results
     evalRes <- data.frame(OA = OA, kappa = kappa)
