d956f07d3b2c532a420c7e6e70c99115863d6bb2
Alexandre Wadoux <33750884+AlexandreWadoux@users.noreply.github.com>
2020-04-29 18:44:45 +1000

    Update chBLCext.R

--- R/chBLCext.R
+++ R/chBLCext.R
@@ -8,41 +8,38 @@
 #'
 #' @export
 
-#Convex hull function used for regions of interest work such as detecting secondary clay minerals from vis-NIR spectra
-chBLCext <- function(spectra, lower, upper, ...){
-  interval <- c(1:ncol(spectra))
+chBLCext <- function(spectra, lower, upper, type, ...){
+  
+  wav <- as.numeric(colnames(spectra))
   hullSpectra <- matrix(NA, ncol = ncol(spectra), nrow = nrow(spectra))
-  tempSpect = as.matrix(spectra)
-  data1 <- sortedXyData(interval, tempSpect)
+   tempSpect = as.matrix(spectra)
+  # data1 <- sortedXyData(wav, tempSpect)
+  
+  X <- spectra
+  cHullfun <- function(x, wav) {
+    id <- sort(chull(c(wav[1] - 1, wav, wav[length(wav)] + 1), c(0, x, 0)))
+    cHull <- id[-c(1, length(id))] - 1
+    return(cHull)
+  }
+  
+  if (type == "A") {
+    X <- 1/X
+}
 
-  ## calculate convex hull
-  cHull <- chull(data1)
-  ## get the appropriate region
-  cc <-cHull
-  xs <- which(cc == 1)
-  ccd <- cc[which(cc == 1):length(cc)]
-  ccdM <- matrix(NA, nrow = 1, ncol = length(ccd))
-  for(f in 1:(length(ccd)-1)){
-    if(ccd[f] < ccd[f+1]){ccdM[1,f] <- ccd[f]}
-    else {ccdM[1,f] <- ccd[f]
-    break}}
-  if(f == (length(ccd)-1)){ccdM[1,ncol(ccdM)] <- ccd[length(ccd)]}
-  tempX <- c(ccdM)[!is.na(c(ccdM))]
-  #Left side
-  if(cHull[1] > cHull[length(cHull)]){lss <- cc[1:(xs - 1)]
-  slss <- lss[which(lss >= lss[1])]
-  cHull <- sort(c(tempX, slss))} else {cHull <- tempX}
+cHull <- cHullfun(X, wav)
 
-  ## calculate linear approximation between hull points
-  linearApprox <- approx(data1[cHull,], xout = interval, method = 'linear', ties = 'mean')
-  ## calculate the deviation from the convex hull
-  hullSpectra[1,] <- 1- (( linearApprox[[2]] - tempSpect )/linearApprox[[2]])
-  colnames(hullSpectra) <- colnames(spectra)
-  # construct polygon
-  aaa <- hullSpectra
-  aaa[,c(1,ncol(aaa))] <- 1.0001
-  bbb <- cbind(seq(lower, upper, by = 1), t(aaa))
-  ccc <- cbind(seq(lower, upper,by = 1), rep(c(1.0001),each = length(seq(lower, upper, by = 1))))
-  ddd <- as.matrix(rbind(bbb, ccc))
-  retval <- list(wave = seq(lower, upper, by = 1),cHull = hullSpectra, rawSpec = data1$y,continuum = linearApprox[[2]], polygon = ddd)
-  return(retval)}
+## calculate linear approximation between hull points
+linearApprox <- approx(x = wav[cHull], y = tempSpect[cHull] , xout = wav, method = 'linear', ties = 'mean')
+## calculate the deviation from the convex hull
+hullSpectra[1,] <- 1 - (( linearApprox[[2]] - tempSpect )/linearApprox[[2]])
+colnames(hullSpectra) <- wav
+# construct polygon
+aaa <- hullSpectra
+aaa[,c(1,ncol(aaa))] <- 1.0001
+bbb <- cbind(wav, t(aaa))
+ccc <- cbind(wav, rep(c(1.0001), each = length(wav)))
+ddd <- as.matrix(rbind(bbb, ccc))
+retval <- list(wave = wav, cHull = hullSpectra, rawSpec = tempSpect, continuum = linearApprox[[2]], polygon = ddd)
+return(retval)
+
+}
