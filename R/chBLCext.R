93ae715082c8bae2fbae7ab52be9aab9849038f9
Alexandre Wadoux <33750884+AlexandreWadoux@users.noreply.github.com>
2020-05-02 13:15:36 +1000

    Update chBLCext.R

--- R/chBLCext.R
+++ R/chBLCext.R
@@ -8,38 +8,48 @@
 #'
 #' @export
 
-chBLCext <- function(spectra, lower, upper, type, ...){
-  
-  wav <- as.numeric(colnames(spectra))
-  hullSpectra <- matrix(NA, ncol = ncol(spectra), nrow = nrow(spectra))
-   tempSpect = as.matrix(spectra)
-  # data1 <- sortedXyData(wav, tempSpect)
+chBLCext<- function(spectra, type = c("R", "A"), wav, ...){
   
   X <- spectra
-  cHullfun <- function(x, wav) {
-    id <- sort(chull(c(wav[1] - 1, wav, wav[length(wav)] + 1), c(0, x, 0)))
-    cHull <- id[-c(1, length(id))] - 1
-    return(cHull)
+  
+  # if absorbance instead take the inverse to find the CH points
+  if (type == "A"){X <- 1/X}
+  
+  cHullFun <- function(x, wav, interpol) {
+    cHull <- sort(chull(c(wav[1] - 1, wav, wav[length(wav)] + 1), c(0, x, 0)))
+    cHull <- cHull[-c(1, length(cHull))] - 1
+    cont <- approx(x = wav[cHull], y = x[cHull], xout = wav, method = "linear")$y
+    return(cont)
   }
   
-  if (type == "A") {
-    X <- 1/X
+  cont <- cHullFun(X, wav, interpol)
+  
+  if (type == "A"){ 
+    # substraction - absorbance (Fig. 5 Clark & Roush (1984))
+    hullSpectra <-  1 + X - cont 
+  }else{
+    # division - reflectance (Fig. 5 Clark & Roush (1984))
+    hullSpectra <-  X/cont  
+  }
+  
+  if (type == "A"){ 
+    # back transform the CR spectra
+    hullSpectra <- 1/hullSpectra  
+    # back transform to show the CH line
+    cont <- 1/cont 
+    # back transformt the spectra
+    X <- 1/X 
+  }
+  
+  # prepare xy for the polygon
+  pol <- cbind(wav, as.numeric(hullSpectra[1,]))
+  
+  
+  retval<- list(wave = wav, 
+                cHull = hullSpectra, 
+                rawSpec = X, 
+                continuum = cont, 
+                polygon = pol)
+  return(retval)
 }
 
-cHull <- cHullfun(X, wav)
-
-## calculate linear approximation between hull points
-linearApprox <- approx(x = wav[cHull], y = tempSpect[cHull] , xout = wav, method = 'linear', ties = 'mean')
-## calculate the deviation from the convex hull
-hullSpectra[1,] <- 1 - (( linearApprox[[2]] - tempSpect )/linearApprox[[2]])
-colnames(hullSpectra) <- wav
-# construct polygon
-aaa <- hullSpectra
-aaa[,c(1,ncol(aaa))] <- 1.0001
-bbb <- cbind(wav, t(aaa))
-ccc <- cbind(wav, rep(c(1.0001), each = length(wav)))
-ddd <- as.matrix(rbind(bbb, ccc))
-retval <- list(wave = wav, cHull = hullSpectra, rawSpec = tempSpect, continuum = linearApprox[[2]], polygon = ddd)
-return(retval)
-
-}
